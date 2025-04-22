12f13c8cdb9000d6a553d4af99aca404fd9f1019
Lukas Kurth <lukas98kurth@protonmail.com>
2025-04-22 19:43:41 +0200

    alternative design in PdL Color

--- html/static/script.js
+++ html/static/script.js
@@ -1,23 +1,3 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const board = document.querySelector('.board');
-    const flyers = Array.from(board.children);
-
-    // Zufällige Reihenfolge
-    flyers.sort(() => Math.random() - 0.5);
-
-    // Neu anordnen
-    flyers.forEach(flyer => board.appendChild(flyer));
-
-    // Zufällige Verzögerung und Animation für jedes Flyer
-    flyers.forEach(flyer => {
-        const delay = (Math.random() * 4).toFixed(2); // 0–4 Sekunden
-        const duration = (3 + Math.random() * 2).toFixed(2); // 3–5 Sekunden
-
-        flyer.style.animation = `wobble ${duration}s ease-in-out ${delay}s infinite alternate`;
-    });
-});
-
-
 class FlyerLink extends HTMLElement {
     static get observedAttributes() {
         return ['href', 'icon', 'label', 'random', 'colorclass', 'title'];
@@ -39,15 +19,20 @@ class FlyerLink extends HTMLElement {
         const href = this.getAttribute('href') || '#';
         const icon = this.getAttribute('icon') || 'fas fa-link';
         const label = this.getAttribute('label') || 'Link';
-        const random = this.getAttribute('random') || '0.5';
-        const colorClass = this.getAttribute('colorclass') || '';
         const title = this.getAttribute('title') || '';
+        const offset = this.getAttribute('offset') || "false";
+
+        var margin = offset === "true" ? Math.random() * (15 - 5) + 5 : 0;
+        var direction =  Math.random() < 0.5 ? '-' : '';
+
 
         this.innerHTML = `
-        <a class="flyer ${colorClass}" href="${href}" target="_blank" title="${title}" style="--random: ${random}">
+        <div class="flyer-wrapper" style="margin-left: ${direction}${margin}%">
+        <a class="flyer" href="${href}" target="_blank" title="${title}">
           <i class="${icon}"></i>
           ${label}
         </a>
+        </div>
       `;
     }
 }
